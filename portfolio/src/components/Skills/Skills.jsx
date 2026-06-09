bb225450b4d8df6bd8e65b4c94c8a49b04a50e6d
Guillaume Bzh <guillaume4cebil@gmail.com>
2026-06-09 19:46:23 +0200

    feat(Skills) add skills in progress

--- portfolio/src/components/Skills/Skills.jsx
+++ portfolio/src/components/Skills/Skills.jsx
@@ -1,13 +1,30 @@
+import "./Skills.scss";
 function Skills() {
     return (
         <div className="skills">
-            <h2>Compétences</h2>
-            <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>React</li>
-                <li>Git</li>
+            <h2 className="skills__title">Compétences</h2>
+            <ul className="skills__list">
+                <li className="skills__item html">
+                    <i className="fa-brands fa-html5"></i>
+                    <p className="hidden">HTML</p>
+                </li>
+                <li className="skills__item css">
+                    <i className="fa-brands fa-css3-alt"></i>
+                    <p className="hidden">CSS</p>
+                </li>
+                <li className="skills__item git">
+                    <i className="fa-brands fa-github"></i>
+                    <p className="hidden">Git</p>
+                </li>
+                <li className="skills__item react">
+                    <i className="fa-brands fa-react"></i>
+                    <p className="hidden">React</p>
+                </li>
+
+                <li className="skills__item javascript">
+                    <i className="fa-brands fa-js"></i>
+                    <p className="hidden">JavaScript</p>
+                </li>
             </ul>
         </div>
     );
