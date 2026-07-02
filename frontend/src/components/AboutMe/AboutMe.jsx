8d32d1c0c88fbe6e987fb4aef40fdde4c760d45f
Guillaume Bzh <guillaume4cebil@gmail.com>
2026-07-02 14:06:07 +0200

    refractor(about) text about

--- frontend/src/components/AboutMe/AboutMe.jsx
+++ frontend/src/components/AboutMe/AboutMe.jsx
@@ -12,6 +12,12 @@ function About() {
                 J'apprends tous les jours, je pose beaucoup de questions et je préfère rendre un travail dont je suis fier plutôt que de bâcler les choses. Et même si j'aime plaisanter au quotidien,
                 je sais aussi être très sérieux quand un projet le demande.
             </p>
+
+            <p>Toujours animé par l'envie de progresser, je développe actuellement mes compétences en Java afin d'élargir mon profil vers le développement full-stack.</p>
+
+            <p>
+                Je suis actuellement à la recherche de ma première opportunité en tant que développeur web junior et serais ravi de rejoindre une équipe avec laquelle apprendre, contribuer et évoluer.
+            </p>
         </div>
     );
 }
