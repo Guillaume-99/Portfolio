8aae9a73679736a90e18fce48709ee1655d633e2
Guillaume Bzh <guillaume4cebil@gmail.com>
2026-07-02 13:55:38 +0200

    refractor(about) text about

--- frontend/src/components/AboutMe/AboutMe.jsx
+++ frontend/src/components/AboutMe/AboutMe.jsx
@@ -4,13 +4,13 @@ function About() {
         <div className="aboutMe">
             <h2>À propos de moi</h2>
             <p>
-                Fraîchement diplômé et reconverti en développeur front-end junior, je suis un passionné de React — un peu comme une rockstar du code, mais sans les groupies. Rigoureux et motivé, je
-                préfère un travail soigné quitte à y passer un peu plus de temps, car qualité rime avec fierté (et pas seulement avec café renversé).
+                Fraîchement diplômé et reconverti en développeur front-end junior, je suis passionné par React et par la création d'interfaces modernes, intuitives et agréables à utiliser. J'aime que
+                mon code soit comme une bonne playlist : fluide, cohérent et sans fausse note... Bon, il arrive qu'un bug improvise un solo, mais ça fait partie du métier.
             </p>
 
             <p>
-                Fan de musique, j’aime que mon code soit aussi harmonieux qu’une bonne playlist. Pas là pour me vendre, juste un dev junior qui cherche à contribuer à des projets variés, avec
-                enthousiasme et minutie. React, c’est mon coup de cœur, simple et efficace, comme moi !
+                J'apprends tous les jours, je pose beaucoup de questions et je préfère rendre un travail dont je suis fier plutôt que de bâcler les choses. Et même si j'aime plaisanter au quotidien,
+                je sais aussi être très sérieux quand un projet le demande.
             </p>
         </div>
     );
